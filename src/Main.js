b7e5b6c31e67cba268355ab8b9c86915fafa0955
Jake Harris <harri676@purdue.edu>
2019-04-16 20:13:49 -0400

    Changes list of projects to use components

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
 import TopBar from './TopBar';
+import ProjectBox from './ProjectBox'
 
 class Main extends Component {
+
+    constructor(){
+        super()
+
+        this.state = {
+            projects: [
+                {
+                    title: "Service Project 1",
+                    author: "Jake Harris",
+                    desc: "This is service project test 1",
+                    num_people: "1",
+                    date: "May 1st, 2019",                    
+                },
+                {
+                    title: "Service Project 2",
+                    author: "Jake Harris",
+                    desc: "This is service project test 2",
+                    num_people: "3",
+                    date: "May 7th, 2019",                    
+                },
+                {
+                    title: "Service Project 3",
+                    author: "Jake Harris",
+                    desc: "This is service project test 3",
+                    num_people: "2",
+                    date: "May 15th, 2019",                    
+                },
+            ],
+        }
+    }
+
     render(){
         return(
             <div className="Main" style={styles.main}>
                 <TopBar />
                 <div className="Body" style={styles.body}>
-                    <dl style={styles.list}>
+                    {/* <dl style={styles.list}>
                         <div style={styles.dtd}>
                             <dt>Service Project 1</dt>
                             <dd>- Description 1</dd>
@@ -20,7 +52,10 @@ class Main extends Component {
                             <dt>Service Project 3</dt>
                             <dd>- Description 3</dd>
                         </div>
-                    </dl>
+                    </dl> */}
+                    {
+                        this.state.projects.map(prj => <ProjectBox project={prj} />)
+                    }
                 </div>
             </div>
         )
@@ -30,32 +65,15 @@ class Main extends Component {
 const styles = {
     body: {
         backgroundColor: 'blue',
-        height: '100vh',
-    },
-    dtd: {
-        backgroundColor: "grey",
-        padding: "2rem, 2rem",
-        margin: "1rem",
-        display: 'flex',
-        flexDirection: 'column',
-        borderRadius: '0.5rem',
-        borderColor: 'black',
-        borderWidth: '2px',
-        width: '25rem',
         height: 'auto',
-        fontFamily: 'Trebuchet MS',
-
-        ':hover': {
-            backgroundColor: "white",
-        }
-    },
-    list: {
         alignItems: 'row',
         justifyContent: 'left',
         display: 'flex',
+        flexDirection: 'columns',
     },
     main: {
         backgroundColor: 'blue',
+        height: '100vh',
     },
 }
 
---
+++ src/ProjectBox.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class ProjectBox extends Component{
+
+    render(){
+
+        const project = this.props.project
+
+        return(
+            <div className="ProjectBox" style={styles.dtd}>
+                <h3>{project.title} by {project.author}</h3>
+                <p>{project.desc}</p>
+                <p>Number of people needed: {project.num_people}</p>
+                <p>Project start date: {project.date}</p>
+            </div>
+        )
+    }
+
+}
+
+const styles = {
+    dtd: {
+        backgroundColor: "grey",
+        padding: "2rem, 2rem",
+        margin: "1rem",
+        display: 'flex',
+        flexDirection: 'column',
+        borderRadius: '0.5rem',
+        borderColor: 'black',
+        borderWidth: '2px',
+        width: 'auto',
+        height: 'auto',
+        fontFamily: 'Trebuchet MS',
+    },
+}
+
+export default ProjectBox
