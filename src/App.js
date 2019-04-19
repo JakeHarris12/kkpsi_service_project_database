194d2a73af7de77b3dfe18763234c988e88aade1
Jake Harris <harri676@purdue.edu>
2019-04-18 20:10:34 -0400

    Fixes bug with page refresh

--- src/App.js
+++ src/App.js
@@ -10,146 +10,150 @@ import config from './firebaseConfig'
 
 class App extends Component {
 
-  constructor(props){
-    super(props)
+    constructor(props) {
+        super(props)
 
-    firebase.initializeApp(config)
-    this.db = firebase.firestore()
+        firebase.initializeApp(config)
+        this.db = firebase.firestore()
 
-    this.state = {
-      projects: [
-        {
-            title: "Service Project 1",
-            author: "Jake Harris",
-            desc: "This is service project test 1",
-            num_people: "1",
-            date: "May 1st, 2019",
-        },
-        {
-            title: "Service Project 2",
-            author: "Jake Harris",
-            desc: "This is service project test 2",
-            num_people: "3",
-            date: "May 7th, 2019",
-        },
-        {
-            title: "Service Project 3",
-            author: "Jake Harris, Ian Ostermann",
-            desc: "This is service project test 3",
-            num_people: "2",
-            date: "May 15th, 2019",
-        },
-      ],
+        this.state = {
+            projects: [
+                {
+                    title: "Service Project 1",
+                    author: "Jake Harris",
+                    desc: "This is service project test 1",
+                    num_people: "1",
+                    date: "May 1st, 2019",
+                },
+                {
+                    title: "Service Project 2",
+                    author: "Jake Harris",
+                    desc: "This is service project test 2",
+                    num_people: "3",
+                    date: "May 7th, 2019",
+                },
+                {
+                    title: "Service Project 3",
+                    author: "Jake Harris, Ian Ostermann",
+                    desc: "This is service project test 3",
+                    num_people: "2",
+                    date: "May 15th, 2019",
+                },
+            ],
 
-      displayProjectForm: false,
-      loggedin: false,
-      user:{
-        name:"This shouldn't be here!"
-      },
-    }
-  }
-
-  componentDidMount() {
-      console.log("component mounted")
-      var result = null,
-        tmp = []
-      window.location.search.substring(1).split("&").forEach(function (item) {
-        tmp = item.split("=")
-        if(tmp[0] === "code") result = decodeURIComponent(tmp[1])
-      })
-      if(result !== null){
-        this.handleAuth(result, this.handleCallback)
-      }
-  }
-
-  handleCallback = (response, code) => {
-    var result = JSON.parse(response)
-    if(result.ok === true){
-      this.setState({user: result.user})
-      this.setState({loggedin: true})
-      var data = {
-        name: result.user.name,
-        email: result.user.email,
-        id: result.user.id,
-        team: result.team.name,
-        teamID: result.team.id,
-        teamDomain: result.team.domain,
-        scope: result.scope,
-        ok: result.ok,
-        accessToken: result.access_token,
-      }
-      console.log(result)
-      // var docRef = this.db.collection('Users').doc(result.user.id)
-      // docRef.set(data, {merge: true})
-      localStorage.setItem(`${code}`, JSON.stringify(data))
-    }else{
-      if(result.error === "code_already_used"){
-         var user = JSON.parse(localStorage.getItem(`${code}`))
-         this.setState({user})
-         this.setState({loggedin: true})
-      }else{
-        this.setState({loggedin: false})
-      }
-    }
-  }
-  handleAuth = (code, callback) => {
-    console.log("In handleAuth")
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-      if(xmlHttp.readyState === 4 && xmlHttp.status === 200)
-        callback(xmlHttp.response, code)
-    }
-    xmlHttp.open("GET", `https://slack.com/api/oauth.access?client_id=600357668291.605536749281&client_secret=9f38bf428122b05d8c401893464bba5c&code=${code}&redirect_uri=http%3A%2F%2Flocalhost%3A3000`, true)
-    xmlHttp.send(null)
-  }
-
-  addProject = (project) => {
-    const projects = [...this.state.projects]
-    projects.push(
-        {
-            title: project.title,
-            author: project.author,
-            desc: project.desc,
-            num_people: project.num_people,
-            date: project.date,
+            displayProjectForm: false,
+            loggedin: false,
+            user: {
+                name: "This shouldn't be here!"
+            },
         }
-    )
-    this.setState({ projects })
-    this.displayProjectForm()
-  }
-
-  displayProjectForm = () => {
-    this.setState({
-        displayProjectForm: !this.state.displayProjectForm
-    })
-  }
-
-  render() {
-
-    const loggedin = this.state.loggedin
-    if(!loggedin){
-      return(
-        <div className="App">
-          <Login/>
-        </div>
-      )
-    }else{
-      const displayProjectForm = this.state.displayProjectForm
-      let page;
-
-      if(displayProjectForm){
-        page = <ProjectForm displayProjectForm={this.displayProjectForm} addProject={this.addProject}/>
-      }else{
-        page = <Main projects={this.state.projects} displayProjectForm={this.displayProjectForm} user={this.state.user}/>
-      }
-
-      return (
-        <div className="App">
-          {page}
-        </div>
-      );
     }
-  }
+
+    componentDidMount() {
+        console.log("component mounted")
+        var result = null,
+            tmp = []
+        window.location.search.substring(1).split("&").forEach(function (item) {
+            tmp = item.split("=")
+            if (tmp[0] === "code") result = decodeURIComponent(tmp[1])
+        })
+        if (result !== null) {
+            this.handleAuth(result, this.handleCallback)
+        }
+    }
+
+    handleCallback = (response, code) => {
+        var result = JSON.parse(response)
+        if (result.ok === true) {
+            this.setState({ user: result.user })
+            this.setState({ loggedin: true })
+            var data = {
+                name: result.user.name,
+                email: result.user.email,
+                id: result.user.id,
+                team: result.team.name,
+                teamID: result.team.id,
+                teamDomain: result.team.domain,
+                scope: result.scope,
+                ok: result.ok,
+                accessToken: result.access_token,
+            }
+            console.log(result)
+            // var docRef = this.db.collection('Users').doc(result.user.id)
+            // docRef.set(data, {merge: true})
+            localStorage.setItem(`${code}`, JSON.stringify(data))
+        } else {
+            if (result.error === "code_already_used") {
+                var user = JSON.parse(localStorage.getItem(`${code}`))
+                if (user !== null) {
+                    this.setState({ user })
+                    this.setState({ loggedin: true })
+                } else {
+                    this.setState({ loggedin: false })
+                }
+            } else {
+                this.setState({ loggedin: false })
+            }
+        }
+    }
+    handleAuth = (code, callback) => {
+        console.log("In handleAuth")
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+                callback(xmlHttp.response, code)
+        }
+        xmlHttp.open("GET", `https://slack.com/api/oauth.access?client_id=600357668291.605536749281&client_secret=9f38bf428122b05d8c401893464bba5c&code=${code}&redirect_uri=http%3A%2F%2Flocalhost%3A3000`, true)
+        xmlHttp.send(null)
+    }
+
+    addProject = (project) => {
+        const projects = [...this.state.projects]
+        projects.push(
+            {
+                title: project.title,
+                author: project.author,
+                desc: project.desc,
+                num_people: project.num_people,
+                date: project.date,
+            }
+        )
+        this.setState({ projects })
+        this.displayProjectForm()
+    }
+
+    displayProjectForm = () => {
+        this.setState({
+            displayProjectForm: !this.state.displayProjectForm
+        })
+    }
+
+    render() {
+
+        const loggedin = this.state.loggedin
+        if (!loggedin) {
+            return (
+                <div className="App">
+                    <Login />
+                </div>
+            )
+        } else {
+            const displayProjectForm = this.state.displayProjectForm
+            let page;
+
+            if (displayProjectForm) {
+                page = <ProjectForm displayProjectForm={this.displayProjectForm} addProject={this.addProject} />
+            } else {
+                page = <Main projects={this.state.projects} displayProjectForm={this.displayProjectForm} user={this.state.user} />
+            }
+
+            return (
+                <div className="App">
+                    {page}
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
