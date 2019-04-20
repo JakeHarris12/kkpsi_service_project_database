6f70da7fe94e632e9681d4a11646b6d3f1f47ad3
Ian <ian@ostermann.me>
2019-04-19 20:01:35 -0400

    Convert Login screen to Aphrodite

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
+import {css, StyleSheet} from "aphrodite";
 
 class Login extends Component{
 
     render(){
         return(
-            <div className="Login" style={styles.login}>
-                <h1 style={styles.h}>Welcome to the Kappa Kappa Psi Gamma Pi Service Project Database</h1>
-                <h3 style={styles.h}>Please Sign in Below:</h3>
+            <div className={css(styles.login)}>
+                <h1 className={css(styles.h)}>Welcome to the Kappa Kappa Psi Gamma Pi Service Project Database</h1>
+                <h3 className={css(styles.h)}>Please Sign in Below:</h3>
                 <a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=600357668291.605536749281&redirect_uri=http%3A%2F%2Flocalhost%3A3000">
-                    <img alt="Sign in with Slack" 
-                        height="40" 
-                        width="172" 
+                    <img alt="Sign in with Slack"
                         src="https://platform.slack-edge.com/img/sign_in_with_slack.png" 
-                        srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" 
+                        srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
+                         className={css(styles.image)}
                     />    
                 </a>
             </div>
@@ -20,7 +20,7 @@ class Login extends Component{
     }
 }
 
-const styles = {
+const styles = StyleSheet.create({
     h:{
         color: 'blue',
         margin: '0',
@@ -39,6 +39,10 @@ const styles = {
         borderRadius: '1rem',
         border: '2px solid grey',
     },
-}
+    image: {
+        height: "40px",
+        width: "172px",
+    }
+})
 
 export default Login
